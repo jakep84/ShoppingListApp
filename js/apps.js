96eb69d5fab761a74565567ffaad11574c18bf4d
Jake Priddy <priddyjacob84@gmail.com>
2016-02-24 15:38:49 -0500

    2/24 1538

---
+++ js/apps.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	// $("#btn2").click(function(){
+ //     $("ol").append("<li>Appended item</li>");
+ //    });
+	$("#btn_AddToList").mouseenter(function(){
+		alert("something");
+   //alert($('input[name=List1]').val());  
+    //It'll let you know the textbox's value
+    // $('input[name=List1]').val().appendTo('#List2');
+    });         
+});
+// </script> 
+// $(document).ready(function() {
+//   $('.ryu').mouseenter(function() {
+//     alert('mouse entered .ryu div');
+//   });
+// });
