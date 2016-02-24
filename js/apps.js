fa05fee9786ae570ac38272ba2ad99a56efae76d
Jake Priddy <priddyjacob84@gmail.com>
2016-02-24 16:29:52 -0500

    2/24 1629

--- js/apps.js
+++ js/apps.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-	// $("#btn2").click(function(){
- //     $("ol").append("<li>Appended item</li>");
- //    });
-	$("#btn_AddToList").mouseenter(function(){
-		alert("something");
+  $('.btn_AddToList').onclick(function() {
+    alert('mouse entered .btn_AddToList input');
+  });
+});
+// $(document).ready(function() {
+// 	// $("#btn2").click(function(){
+//  //     $("ol").append("<li>Appended item</li>");
+//  //    });
+// 	$("#btn_AddToList").mouseenter(function(){
+// 		alert("something");
    //alert($('input[name=List1]').val());  
     //It'll let you know the textbox's value
     // $('input[name=List1]').val().appendTo('#List2');
-    });         
-});
+//     });         
+// });
+
+// make on click price come from embeded list 
+//to make it com out to the title line 
+// put product entered from textbox onto list   
+
+
+
+
 // </script> 
 // $(document).ready(function() {
 //   $('.ryu').mouseenter(function() {
